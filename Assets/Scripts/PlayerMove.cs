6443de23857e5707776477b45e056e125bde13e6
TERAfenine <138286124+TERAfenine@users.noreply.github.com>
2025-05-30 20:38:18 +0900

    1

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -13,15 +13,17 @@ public class PlayerMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    
+
     void FixedUpdate()
     {
+        //Move By Key Control
         float h = Input.GetAxisRaw("Horizontal");
-
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
-        if (rigid.linearVelocity.x > maxSpeed)
+        if (rigid.linearVelocity.x > maxSpeed) //Right Max Speed
             rigid.linearVelocity = new Vector2(maxSpeed, rigid.linearVelocity.y);
-        else if (rigid.linearVelocity.x < maxSpeed * (-1))
+        else if (rigid.linearVelocity.x < maxSpeed * (-1)) //Left Max Speed
             rigid.linearVelocity = new Vector2(maxSpeed * (-1), rigid.linearVelocity.y);
     }
 }
