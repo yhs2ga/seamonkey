50177212c86c49fed719376f4bb7b7b68f802d85
TERAfenine <yahkoma@gmail.com>
2025-06-13 16:11:56 +0900

    .

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,18 +8,33 @@ public class PlayerMove : MonoBehaviour
     public float maxSpeed;
     public float jumpPower;
     Rigidbody2D rigid;
+    SpriteRenderer spr;
+
+    private int direction = -1;
+    private bool isJump = false;
+    private bool isAttack = false;
+
+    [SerializeField] private GameObject weapon;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.X) && !isAttack)
+        {
+            isAttack = true;
+            StartCoroutine(asdf());
+    
+        }
         //Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJump)
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isJump = true;
         }
     }
 
@@ -33,5 +48,36 @@ public class PlayerMove : MonoBehaviour
             rigid.linearVelocity = new Vector2(maxSpeed, rigid.linearVelocity.y);
         else if (rigid.linearVelocity.x < maxSpeed * (-1)) //Left Max Speed
             rigid.linearVelocity = new Vector2(maxSpeed * (-1), rigid.linearVelocity.y);
+        
+        if (h == 0) rigid.linearVelocity = new Vector2(0, rigid.linearVelocityY);
+
+        if (h == 1 && !isAttack)
+        {
+            spr.flipX = true;
+            weapon.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            direction = 1;
+        }
+        else if (h == -1 && !isAttack)
+        {
+            spr.flipX = false;
+            weapon.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            direction = -1;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJump = false;
+        }
+    }
+
+    IEnumerator asdf()
+    {
+        weapon.transform.position = new Vector2(weapon.transform.position.x + 1 * direction, weapon.transform.position.y);
+        yield return new WaitForSeconds(0.1f);
+        weapon.transform.position = new Vector2(weapon.transform.position.x - 1 * direction, weapon.transform.position.y);
+        isAttack = false;
     }
 }
